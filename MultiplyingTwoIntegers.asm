86aa3b65a9c55ab3b879e9f70b527724a52fe7e4
whoisxnio <enioblvck@gmail.com>
2022-06-01 00:04:27 +0100

    Assembly Mips
    Multiplying Two Integers

--- MultiplyingTwoIntegers.asm
+++ MultiplyingTwoIntegers.asm
@@ -1,9 +1,10 @@
-.text 
+.text #main section
 
-	li $t0, 5
-	li $t1, 10
-	mul $s0, $t0, $t1
-	li $v0, 1
-	move $a0, $s0
+	li $t0, 5 #passing the value 5 to register $t0
+	li $t1, 10 #passing the value 10 to register $t1
+	mul $s0, $t0, $t1 #multiplying $t0 by $t1 and storing in $s0
+	li $v0, 1 #setting the code to print an integer
+	move $a0, $s0 #moving the result to $a0 in order to be printed out
 
-syscall
+syscall #triggering the execution
+
