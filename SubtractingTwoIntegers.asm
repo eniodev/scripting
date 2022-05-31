b5339a87772ee911405ba9e2f3ac07de021c6ab3
whoisxnio <enioblvck@gmail.com>
2022-06-01 00:02:28 +0100

    Asssembly Mips
    Subtracting Two Integers

---
+++ SubtractingTwoIntegers.asm
@@ -0,0 +1,9 @@
+.text #main section
+
+	li $t0, 5 #passing the value 5 to register $t0
+	li $t1, 10 #passing the value 10 to register $t1
+	add $s0, $t0, $t1 #subtracting $t1 on $t0 and storing in $s0
+	li $v0, 1 #setting the code to print an integer
+	move $a0, $s0 #moving the result to $a0 in order to be printed out
+
+syscall #triggering the execution
