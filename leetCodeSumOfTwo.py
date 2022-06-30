e9d61b05d35cfa0625f42cb0d5a057102bc14762
whoisxnio <enioblvck@gmail.com>
2022-06-30 02:37:26 +0100

    fist try at leetcode + accepted

---
+++ leetCodeSumOfTwo.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        tam = len(nums)
+
+        if(tam == 2):
+            return [0, 1]
+        else:
+            i = 0
+            j = 1
+            while((nums[i]+nums[j]) != target and i != tam-2):
+                if(j == tam-1):
+                    j = i + 1
+                    i = i + 1
+                j = j + 1
+        return [i, j]
